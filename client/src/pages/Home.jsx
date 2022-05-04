3dfdfc56b54825bf7e8d3d00111181919a1a0ca4
MadSamson <m.mahdi.samadi@gmail.com>
2022-05-04 16:40:24 +0200

    add home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {useState, useEffect} from 'react'
+
+
+export default function Home() {
+    const [userData, setUserData] = useState('')
+    const [toDos, setToDos] = useState('')
+    
+    useEffect(()=>{
+        getUndoneToDo()
+    },[])
+
+    function getUndoneToDo() {
+        const token = localStorage.getItem('ToDoAssignment')
+        const headers = {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        };
+        const url = 'http://localhost:4000/todo/listToDo'
+        fetch(url, {
+          method: "GET",
+          headers: headers,
+        }).then(res => res.json())
+        .then(data => console.log(data))
+        
+    }
+
+  return (
+    <div>Home</div>
+  )
+}
