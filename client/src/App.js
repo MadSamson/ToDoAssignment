3c0a6ddb27ff203717d5d65d1b0ac96cf5d092c8
MadSamson <m.mahdi.samadi@gmail.com>
2022-05-04 17:29:17 +0200

    add home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import {Routes, Route} from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register'
+import Home from './pages/Home'
 
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/home" element={<Home/>}/>
     </Routes>
   );
 }
