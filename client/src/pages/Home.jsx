910cc0672fed1b8e3f2ba0ccf1edb9757d9a0e6e
MadSamson <m.mahdi.samadi@gmail.com>
2022-05-10 13:21:22 +0200

    add createToDo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,8 +4,8 @@ import { useNavigate } from 'react-router-dom'
 
 
 export default function Home() {
-    const [userData, setUserData] = useState('')
-    const [toDos, setToDos] = useState('')
+    const [toDosList, setToDosList] = useState('')
+    const [toDo, setToDo] = useState('')
 
     const navigate = useNavigate()
 
@@ -16,19 +16,41 @@ export default function Home() {
     function getUndoneToDo() {
         const token = localStorage.getItem('ToDoAssignment')
         const headers = {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-        };
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        }
         const url = 'http://localhost:4000/todo/listToDo'
         fetch(url, {
-          method: "GET",
+          method: 'GET',
           headers: headers,
         })
         .then(res=>res.json())
         .then(data => {
-          setToDos(data)
+          setToDosList(data)
         })
     }
+
+    function handleOnSubmit(e) {
+      e.preventDefault()
+      const payload = { 'description': toDo}
+      const token = localStorage.getItem('ToDoAssignment')
+      const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      }
+      const url = 'http://localhost:4000/todo/create'
+
+      fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(payload)
+      }).then(res => res.json())
+      .then(data => {
+        console.log(data)
+        getUndoneToDo()
+      })
+    }
+    
     function logout() {
       localStorage.removeItem('ToDoAssignment')
       navigate('/login')
@@ -36,17 +58,19 @@ export default function Home() {
 
   return (
     <>
-    <button onClick={logout}>logout</button>
-    <div>Home</div>
-    {toDos && toDos.map((item) => {
-      return (
-        <div key='{item.id}'>
-          {item.by}
-          <br />
-          {item.description}
-        </div>
-      )
-    })}
+      <button onClick={logout}>logout</button>
+      <form onSubmit={handleOnSubmit}>
+        Add a new task: <input type="text" value={toDo} onChange={e => setToDo(e.target.value)}/>
+        <input type='submit' value='Create'/>
+      </form>
+
+      {toDosList && toDosList.map((item) => {
+        return (
+          <ul key={item._id}>
+            <li>{item.description}</li>
+          </ul>
+        )
+      })}
     </>
     
   )
