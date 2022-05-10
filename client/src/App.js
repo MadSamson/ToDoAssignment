fd90252c213ca22adcc4587ed3a8b1728e878f0f
MadSamson <m.mahdi.samadi@gmail.com>
2022-05-10 13:23:29 +0200

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register'
 import Home from './pages/Home'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/home" element={<Home/>}/>
-    </Routes>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
+      </Routes>
+    </div>
   );
 }
 
